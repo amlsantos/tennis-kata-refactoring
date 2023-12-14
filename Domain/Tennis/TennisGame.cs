cd1c4f19f08c2ebc5084d284f6aa6d69ee34a4ca
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-14 15:13:01 +0000

    extract methods

--- Domain/Tennis/TennisGame.cs
+++ Domain/Tennis/TennisGame.cs
@@ -2,8 +2,8 @@
 
 public class TennisGame : ITennisGame
 {
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private string player1Name;
     private string player2Name;
 
@@ -16,67 +16,88 @@ public class TennisGame : ITennisGame
     public void WonPoint(string playerName)
     {
         if (playerName == "player1")
-            m_score1 += 1;
+            player1Score += 1;
         else
-            m_score2 += 1;
+            player2Score += 1;
     }
 
     public string GetScore()
     {
         var score = "";
-        if (m_score1 == m_score2) // tie
+        if (player1Score == player2Score) // tie
         {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
+            score = DetermineDrawResult();
         }
-        else if (m_score1 >= 4 || m_score2 >= 4) // advantage or win
+        else if (player1Score >= 4 || player2Score >= 4) // advantage or win
         {
-            var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            score = DetermineAdvantageOrWinResult();
         }
         else // 
         {
-            var tempScore = 0;
-            for (var i = 1; i < 3; i++)
-            {
-                if (i == 1) tempScore = m_score1;
-                else
-                {
-                    score += "-";
-                    tempScore = m_score2;
-                }
+            score = DetermineOngoingScore(score);
+        }
 
-                switch (tempScore)
-                {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+        return score;
+    }
+
+    private string DetermineDrawResult()
+    {
+        string score;
+        switch (player1Score)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    private string DetermineAdvantageOrWinResult()
+    {
+        string score;
+        var minusResult = player1Score - player2Score;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    private string DetermineOngoingScore(string score)
+    {
+        var tempScore = 0;
+        for (var i = 1; i < 3; i++)
+        {
+            if (i == 1) tempScore = player1Score;
+            else
+            {
+                score += "-";
+                tempScore = player2Score;
+            }
+
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
         }
 
