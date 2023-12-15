b7398efa956ee38ce9fe4d7f0b6e44ea730a0372
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-15 17:57:57 +0000

    using Player object

--- Domain/Tennis/Arbiter.cs
+++ Domain/Tennis/Arbiter.cs
@@ -15,16 +15,19 @@ public static class Arbiter
 
     private static bool IsTie(int player1Score, int player2Score, Player player1, Player player2)
     {
+        return player1.Score == player2.Score;
         return player1Score == player2Score;
     }
 
     private static bool HasAdvantage(int player1Score, int player2Score, Player player1, Player player2)
     {
+        return player1.Score >= 4 || player2.Score >= 4;
         return player1Score >= 4 || player2Score >= 4;
     }
 
     private static bool IsAheadByOne(int player1Score, int player2Score, Player player1, Player player2)
     {
+        return Math.Abs(player1.Score - player2.Score) == 1;
         return Math.Abs(player1Score - player2Score) == 1;
     }
 }
