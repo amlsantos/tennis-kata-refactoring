7df168c1284f32d9a1a2f81fedf0924e9e94ca64
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-15 18:32:56 +0000

    inline variables

--- Domain/Tennis/Arbiter.cs
+++ Domain/Tennis/Arbiter.cs
@@ -4,27 +4,12 @@ public static class Arbiter
 {
     public static Result DetermineResult(Player player1, Player player2)
     {
-        if (IsTie(player1, player2))
+        if (player1.IsTie(player2))
             return new DrawResult(player1.Score, player2.Score);
-        if (HasAdvantage(player1, player2) && IsAheadByOne(player1, player2))
+        if (player1.HasAdvantage(player2) && player1.IsAheadByOne(player2))
             return new AdvantageResult(player1.Score, player2.Score);
-        if (HasAdvantage(player1, player2))
+        if (player1.HasAdvantage(player2))
             return new WinResult(player1.Score, player2.Score);
         return new OngoingScore(player1.Score, player2.Score);
-    }
-
-    private static bool IsTie(Player player1, Player player2)
-    {
-        return player1.IsTie(player2);
-    }
-
-    private static bool HasAdvantage(Player player1, Player player2)
-    {
-        return player1.HasAdvantage(player2);
-    }
-
-    private static bool IsAheadByOne(Player player1, Player player2)
-    {
-        return player1.IsAheadByOne(player2);
     }
 }
