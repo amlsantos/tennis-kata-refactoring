ceb6a805c08ed62e9711af502123663aaf942710
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-15 15:26:20 +0000

    move to a separate file

--- Domain/Tennis/TennisGame.cs
+++ Domain/Tennis/TennisGame.cs
@@ -28,15 +28,3 @@ public class TennisGame : ITennisGame
             .GetScoreAsText();
     }
 }
-
-public static class Arbiter
-{
-    public static Result DetermineResult(int player1Score, int player2Score)
-    {
-        if (player1Score == player2Score)
-            return new DrawResult(player1Score, player2Score);
-        if (player1Score >= 4 || player2Score >= 4)
-            return new AdvantageOrWinResult(player1Score, player2Score);
-        return new OngoingScore(player1Score, player2Score);
-    }
-}
