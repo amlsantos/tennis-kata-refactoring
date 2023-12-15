015f1e35eff6388a22923c38f93599626ecdb980
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-15 17:24:29 +0000

    add domain terms to Arbiter

--- Domain/Tennis/Arbiter.cs
+++ Domain/Tennis/Arbiter.cs
@@ -4,10 +4,27 @@ public static class Arbiter
 {
     public static Result DetermineResult(int player1Score, int player2Score)
     {
-        if (player1Score == player2Score)
+        if (IsTie(player1Score, player2Score))
             return new DrawResult(player1Score, player2Score);
-        if (player1Score >= 4 || player2Score >= 4)
+        if (HasAdvantage(player1Score, player2Score) && IsAheadByOne(player1Score, player2Score))
             return new AdvantageResult(player1Score, player2Score);
+        if (HasAdvantage(player1Score, player2Score))
+            return new WinResult(player1Score, player2Score);
         return new OngoingScore(player1Score, player2Score);
+    }
+
+    private static bool IsTie(int player1Score, int player2Score)
+    {
+        return player1Score == player2Score;
+    }
+
+    private static bool HasAdvantage(int player1Score, int player2Score)
+    {
+        return player1Score >= 4 || player2Score >= 4;
+    }
+
+    private static bool IsAheadByOne(int player1Score, int player2Score)
+    {
+        return Math.Abs(player1Score - player2Score) == 1;
     }
 }
