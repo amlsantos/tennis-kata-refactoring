bc8ca757eff537d7e97e4f1f1696712c4d5ec7af
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-15 17:20:24 +0000

    split classes

---
+++ Domain/Tennis/AdvantageResult.cs
@@ -0,0 +1,39 @@
+﻿namespace Domain.Tennis;
+
+public class AdvantageResult(int player1Score, int player2Score) : Result(player1Score, player2Score)
+{
+    public override string GetScoreAsText()
+    {
+        var minusResult = Player1Score - Player2Score;
+        switch (minusResult)
+        {
+            case 1:
+                return "Advantage player1";
+            case -1:
+                return "Advantage player2";
+            case >= 2:
+                return "Win for player1";
+            default:
+                return "Win for player2";
+        }
+    }
+}
+
+public class WinResult(int player1Score, int player2Score) : Result(player1Score, player2Score)
+{
+    public override string GetScoreAsText()
+    {
+        var minusResult = Player1Score - Player2Score;
+        switch (minusResult)
+        {
+            case 1:
+                return "Advantage player1";
+            case -1:
+                return "Advantage player2";
+            case >= 2:
+                return "Win for player1";
+            default:
+                return "Win for player2";
+        }
+    }
+}
--- Domain/Tennis/Arbiter.cs
+++ Domain/Tennis/Arbiter.cs
@@ -7,7 +7,7 @@ public static class Arbiter
         if (player1Score == player2Score)
             return new DrawResult(player1Score, player2Score);
         if (player1Score >= 4 || player2Score >= 4)
-            return new AdvantageOrWinResult(player1Score, player2Score);
+            return new AdvantageResult(player1Score, player2Score);
         return new OngoingScore(player1Score, player2Score);
     }
 }
