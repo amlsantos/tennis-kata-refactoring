59fd8bfe3438bc84e13b4c31e79f1191bee8ee4b
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-15 15:11:19 +0000

    add ongoing score

--- Domain/Tennis/TennisGame.cs
+++ Domain/Tennis/TennisGame.cs
@@ -42,6 +42,8 @@ public class TennisGame : ITennisGame
 
     private string DetermineOngoingScore()
     {
+        return new OngoingScore(_player1Score, _player2Score).GetScoreAsText();
+        
         return GetScoreAsString(_player1Score) + "-" + GetScoreAsString(_player2Score);
     }
 
@@ -110,6 +112,34 @@ public class AdvantageOrWinResult(int player1Score, int player2Score) : Result(p
                 return "Win for player1";
             default:
                 return "Win for player2";
+        }
+    }
+}
+
+public class OngoingScore : Result
+{
+    public OngoingScore(int player1Score, int player2Score) : base(player1Score, player2Score)
+    { }
+
+    public override string GetScoreAsText()
+    {
+        return GetScoreAsString(Player1Score) + "-" + GetScoreAsString(Player2Score);
+    }
+    
+    private string GetScoreAsString(int tempScore)
+    {
+        switch (tempScore)
+        {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return string.Empty;
         }
     }
 }
